67ef3ca9627b9b96d7b50239c967557336d67004
Sage Arbor <sagearbor@gmail.com>
2015-10-26 20:57:49 -0400

    Update posts.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,6 @@
 class PostsController < ApplicationController
+  before_action :find_post, only: [:edit, :update]
+
   def index
     @posts = Post.all
   end
@@ -7,8 +9,12 @@ class PostsController < ApplicationController
     @post = Post.new
   end
 
+  def edit
+    #@post = Post.find params[:id]
+  end
+
   def create
-    @post = Post.new params.require(:post).permit(:title, :link)
+    @post = Post.new post_params
 
     if @post.save
       redirect_to posts_path, notice: "Thanks for your post!"
@@ -17,4 +23,29 @@ class PostsController < ApplicationController
       render :new
     end
   end
+
+  def update
+    #@post = Post.find params[:id]
+    if @post.update post_params
+      redirect_to posts_path, notice: "Your changes have been saved"
+    else
+      flash.now[:alert] = @post.errors.full_messages
+      render :edit
+    end
+  end
+
+  private
+
+  def find_post
+    @post = Post.find params[:id]
+  end
+
+  def post_params
+    params.require(:post).permit(:title, :link)
+  end
+
+  #protected
+
+
+
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,6 @@
 class Post < ActiveRecord::Base
   #validates :title, {length: {maxiumu: 255} }
   validates :title, length: {maximum: 255}, presence: true
+  #Map a number to text with enum
+  #enum :post_type: [:link, :text]
 end
